837e69d90f76c435c11020730382c2832d15bbc2
sanj94 <sanjar.haydarov1994@mail.ru>
2023-02-17 18:30:51 +0100

    forms and create.html added

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('article/<str:slug>', views.detail, name='detail')
+    path('article/<str:slug>', views.detail, name='detail'),
+    path('create-post', views.createPost, name='create')
 ]
--- blogsite/views.py
+++ blogsite/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
+from django.utils.text import slugify
 
 
 def index(request):
@@ -12,3 +14,15 @@ def detail(request, slug):
     posts = Post.objects.exclude(post_id__exact=post.post_id)[:5]
     context = {'post': post, 'posts': posts}
     return render (request, 'blogsite/detail.html', context)
+
+def createPost(request):
+    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid:
+            post = form.save(commit=False)
+            post.slug = slugify(post.title)
+            post.save()
+            return redirect('index')
+    context = {'form': form}
+    return render(request, 'blogsite/create.html', context)
